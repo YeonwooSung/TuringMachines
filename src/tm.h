295fb2ae752c1ceccfd6fc0b48b95142e0d61d3e
YeonwooSung <neos960518@gmail.com>
2019-02-28 08:01:27 +0900

    minimal changes

--- src/tm.h
+++ src/tm.h
@@ -1,12 +1,22 @@
 #ifndef TM_H
 #define TM_H
 
+struct state_nondeterministic;
+struct state_deterministic;
+
+
 // transition
 typedef struct transition {
     char inputSymbol;   //character for the input tape symbol
     char outputSymbol;  //character for the output tape symbol
     char *newStateName; //name of the new state
 } Transition;
+
+// transition list
+typedef struct transitionList {
+    Transition *t;
+    struct transitionList *next;
+} TList;
 
 // state for the deterministic turing machine
 typedef struct state_deterministic {
@@ -15,12 +25,6 @@ typedef struct state_deterministic {
     Transition t;
     struct state_deterministic *next;
 } DState;
-
-// transition list
-typedef struct transitionList {
-    Transition *t;
-    struct transitionList *next;
-} TList;
 
 // state for the nondeterministic turing machine
 typedef struct state_nondeterministic {
