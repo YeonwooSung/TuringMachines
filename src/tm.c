31b21127f095e075bdd7ad1b763bea3f89743474
YeonwooSung <neos960518@gmail.com>
2019-03-01 01:52:06 +0900

    add debugging message

--- src/tm.c
+++ src/tm.c
@@ -450,6 +450,7 @@ int main(int argc, char *argv[]) {
             char ret  = run_d(s, tape); //run the turing machine
 
             //TODO free state and transitions
+            printf("%d is the exit code\n", ret);
 
             if (ret) {
                 exit(ret);
