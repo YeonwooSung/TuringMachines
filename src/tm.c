f0d25f5b08094e4233511ebbeae9475125cdaa09
YeonwooSung <neos960518@gmail.com>
2019-02-28 12:55:17 +0900

    handle more input error

--- src/tm.c
+++ src/tm.c
@@ -141,7 +141,22 @@ Alphabets *readDescription(char *filePath, char isDeterministic, State *s) {
         char **splited = splitStr(str, ' ', &counter);
 
         if (strcmp(splited[0], "states") == 0) {
+
+            int a_len = strlen(splited[1]);
+
+            for (int i = 0; i < a_len; i++) {
+                if (!isdigit(splited[1][i])) {
+                    printf("input error\n");
+                    exit(2);
+                }
+            }
+
             num_of_states = atoi(splited[1]);
+
+            if (num_of_states == 0) {
+                printf("input error\n");
+                exit(2);
+            }
         }
     }
 
