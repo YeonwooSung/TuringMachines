2c047f3181f0562f821406bb59ec36821f10a1f9
YeonwooSung <neos960518@gmail.com>
2019-03-03 14:37:52 +0900

    start implementing nondeterministic TM

---
+++ src/tm_n.c
@@ -0,0 +1,25 @@
+#include "tm.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+
+char run_n(State *state, Tape *tape, char entirelyBlank) {
+    size_t num_of_transitions = 0;
+    char virtual_transition = 0;
+    Tape *tapeHead = tape;
+
+
+    // Check if the linked list of tape is null
+    if (tape) {
+        //
+    } else {
+        //
+    }
+
+    char ret = 0;
+
+    return ret;
+}
